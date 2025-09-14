105cc27020dfa3f2dc65bbd1083491bd296c2a8e
mesubby <mesubby@naver.com>
2025-09-15 00:21:42 +0900

    feat: 얼굴 유사도 카드 완성

--- src/components/FaceSimilarityCard.jsx
+++ src/components/FaceSimilarityCard.jsx
@@ -1,10 +1,34 @@
-import styles from './MissingCard.module.css'
+import styles from './FaceSimilarityCard.module.css'
 import { Link } from 'react-router-dom';
 
-export default function FaceSimilarityCard({ originalImage, genImage, name, gender, birth, place, date }) {
+export default function FaceSimilarityCard({ rank, similarity, originalImage, genImage, name, gender, birth, place, date }) {
   return (
     <div>
-
+        <div className={styles.cardContainer}>
+            <div className={styles.cardRank}>
+                <p className={styles.cardNameText}>{rank} 순위</p>
+                <p className={styles.cardNameText}>{similarity} %</p>
+            </div>
+            <div className={styles.cardInfo}>
+                <p className={styles.cardNameText}>{name} ({gender})</p>
+                <div className={styles.cardTextGroup}>
+                    <div className={styles.keyTextGroup}>
+                        <p className={styles.cardKeyText}>생년월일</p>
+                        <p className={styles.cardKeyText}>실종장소</p>
+                        <p className={styles.cardKeyText}>실종날짜</p>
+                    </div>
+                    <div className={styles.valueTextGroup}>
+                        <p className={styles.cardValueText}>{birth}</p>
+                        <p className={styles.cardValueText}>{place}</p>
+                        <p className={styles.cardValueText}>{date}</p>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.cardImageGroup}>
+                <img src={originalImage} className={styles.cardImage} />
+                <img src={genImage} className={styles.cardImage} />
+            </div>
+        </div>
     </div>
   );
 }
--- src/pages/FaceSimilarityPage.jsx
+++ src/pages/FaceSimilarityPage.jsx
@@ -4,11 +4,129 @@ import { Link, useNavigate } from 'react-router-dom';
 import Navbar from "../components/Navbar";
 import Pagination from '../components/Pagination';
 import Searchbar from '../components/Searchbar';
+import FaceSimilarityCard from '../components/FaceSimilarityCard';
 
 import { RiUserSearchFill } from "react-icons/ri";
 import { MdFamilyRestroom } from "react-icons/md";
 
 import styles from './FaceSimilarityPage.module.css';
+
+const userLists = [
+    {
+        id: 0,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '홍길동',
+        gender: '남',
+        birth: '1990년 1월 1일',
+    },
+    {
+        id: 1,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '김철수',
+        gender: '남',
+    },
+    {
+        id: 2,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '박영희',
+        gender: '여',
+    },
+    {
+        id: 3,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '이민호',
+        gender: '남',
+    },
+]; 
+
+const similarityLists= [
+    {
+        id: 1,
+        rank: 1,
+        similarity: 95.2,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '홍길동',
+        gender: '남',
+        birth: '1990년 1월 1일',
+        place: '서울',
+        date: '2023년 10월 26일'
+    },
+    {
+        id: 2,
+        rank: 2,
+        similarity: 90.5,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '김철수',
+        gender: '남',
+        birth: '1991년 2월 2일',
+        place: '부산',
+        date: '2023년 11월 1일'
+    },
+    {
+        id: 4,
+        rank: 3,
+        similarity: 88.7,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '이민호',
+        gender: '남',
+        birth: '1988년 4월 4일',
+        place: '광주',
+        date: '2023년 11월 15일'
+    },
+    {
+        id: 6,
+        rank: 4,
+        similarity: 85.3,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '윤지훈',
+        gender: '남',
+        birth: '1993년 6월 6일',
+        place: '강릉',
+        date: '2023년 11월 25일'
+    },
+    {
+        id: 8,
+        rank: 5,
+        similarity: 82.1,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '정우성',
+        gender: '남',
+        birth: '1987년 8월 8일',
+        place: '수원',
+        date: '2023년 12월 5일'
+    },
+    {
+        id: 11,
+        rank: 6,
+        similarity: 78.9,
+        originalImage: '../public/no-image.jpg',
+        genImage: '../public/no-image.jpg',
+        name: '서준영',
+        gender: '남',
+        birth: '1998년 11월 11일',
+        place: '울산',
+        date: '2023년 12월 20일'
+    },
+]; 
+
+const selectUserCard = {
+    originalImage: '../public/no-image.jpg',
+    genImage: '../public/no-image.jpg',
+    name: '홍길동',
+    gender: '남',
+    birth: '1990년 1월 1일',
+    place: '서울',
+    date: '2023년 10월 26일'
+};
 
 export default function FaceSimilarityPage() {
 
@@ -32,7 +150,36 @@ export default function FaceSimilarityPage() {
                         <button onClick={() => handleTypeClick('실종자')} className={activeType === '실종자' ? styles.typeActivatedBtn : styles.typeBtn}><RiUserSearchFill className={styles.icon}/>실종자</button>
                         <button onClick={() => handleTypeClick('가족')} className={activeType === '가족' ? styles.typeActivatedBtn : styles.typeBtn}><MdFamilyRestroom className={styles.icon}/>가족</button>
                     </div>
-                    <Searchbar/>
+                    <select name='user' id='user' className={styles.select}>
+                        {userLists.map((item) => (
+                            <option value={item.id}>{item.name} ({item.gender})</option>
+                        ))}
+                    </select>
+                </div>
+                <div className={styles.faceSimilarityCardsContainer}>
+                    <div className={styles.selectedUserContainer}>
+                        <p className={styles.cardNameText}>{selectUserCard.name} ({selectUserCard.gender})</p>
+                        <div className={styles.cardTextGroup}>
+                            <div className={styles.keyTextGroup}>
+                                <p className={styles.cardKeyText}>생년월일</p>
+                                <p className={styles.cardKeyText}>실종장소</p>
+                                <p className={styles.cardKeyText}>실종날짜</p>
+                            </div>
+                            <div className={styles.valueTextGroup}>
+                                <p className={styles.cardValueText}>{selectUserCard.birth}</p>
+                                <p className={styles.cardValueText}>{selectUserCard.place}</p>
+                                <p className={styles.cardValueText}>{selectUserCard.date}</p>
+                            </div>
+                        </div>
+                        <img src={selectUserCard.originalImage} className={styles.cardImage} />
+                        <img src={selectUserCard.genImage} className={styles.cardImage} />
+                    </div>
+                    <div className={styles.faceSimilarityCardList}>
+                        {similarityLists.map((item) => (
+                            <FaceSimilarityCard rank={item.rank} similarity={item.similarity} originalImage={item.originalImage} genImage={item.genImage} name={item.name} gender={item.gender} birth={item.birth} place={item.place} date={item.date}></FaceSimilarityCard>  
+                        ))}
+                        <Pagination startPage={1} endPage={5}></Pagination>
+                    </div>
                 </div>
             </div>
         </div>
